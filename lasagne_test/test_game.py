d58787220238f6de0f9cc296adb51c1d87c3bf6f
michał <kempka.michal@gmail.com>
2015-11-08 21:54:37 +0100

    Added short fps test for dot_shooting.
    Former-commit-id: ee690ab51265cd6d0cdfb22f23e2a099fdd77ac0

---
+++ lasagne_test/test_game.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from games import ShootingDotGame
+import time
+import numpy as np
+
+game_args = {}
+game_args['width'] = 320
+game_args['height'] = 200
+game_args['hit_reward'] = 1.0
+game_args['max_moves'] = 50
+#should be positive cause it's treatet as a penalty
+game_args['miss_penalty'] = 0.05
+#should be negative cause it's treatet as a reward
+game_args['living_reward'] = -0.01
+game_args['random_background'] = True
+game_args['ammo'] = np.inf
+game_args['add_dimension'] = True
+game = ShootingDotGame(**game_args)
+
+
+steps = 45000
+start = time.time()
+action = [0,0,0]
+for i in range(steps):
+	if game.is_finished():
+		game.new_episode()
+	s = game.get_state()
+	game.make_action(action)
+
+end = time.time()
+print "time:",round(end-start,4)
+print "fps:",steps/(end-start)
