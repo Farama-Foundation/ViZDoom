7871dbbdb490a5cb0417d09d7b359051a3072d8b
michał <kempka.michal@gmail.com>
2015-11-09 19:24:10 +0100

    Changed run_zdoom.sh to run our scenario.
    Former-commit-id: 8c10d1bbab7c3f33c3dc83c5ba9ab472fe8c341b

--- vizia_doom_src/run_zdoom.sh
+++ vizia_doom_src/run_zdoom.sh
@@ -1,2 +1,2 @@
 #!/bin/bash	
-./zdoom -iwad "doom2.wad" -skill "1" -file "s1.wad" +map MAP01
+./zdoom -iwad "../vizia_doom_scenerio/doom2.wad" -skill "1" -file "../vizia_doom_scenerio/s1.wad" +map MAP01
