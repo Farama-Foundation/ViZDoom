bd283f7dbc3f9ffedb43fb5d3bfd5868003a9d13
michał <kempka.michal@gmail.com>
2015-11-16 18:55:31 +0100

    Added structs for state and state_format.
    Former-commit-id: f80ad739c94195a31a64222b39894f13d2fad76c
    Former-commit-id: 7c5490cea3e4b136e94c9619498765bf7782c007

--- python_port/dot_shooting.h
+++ python_port/dot_shooting.h
@@ -1,13 +1,38 @@
+struct VIZIA_State
+{
+	float* image;
+	float* misc;
+};
+struct VIZIA_StateFormat
+{
+	// number of shapes
+	int image_shape_len;
+	// array of length image_shape_len
+	int* image_shape;
+	// miscallanous vector length
+	int misc_len;
+};
+struct VIZIA_ActionFormat
+{
+	// number of supported keys
+	int keys_num;
+	// wheter mouse delta is supported or not
+	bool mouse_delta;
+};
+
+
+
 void init(int x, int y, int random_bg,int max_moves,double living_reward,double miss_penalty, double hit_reward, int ammo);
 
-int * get_state_format();
+VIZIA_StateFormat* get_state_format();
 int get_action_format();
 
 void new_episode();
 double make_action( int const* action);
 int is_finished();
 double get_summary_reward();
-float * get_image_state();
-float * get_misc_state();
+VIZIA_State* get_state();
+
 
 double average_best_result();
+
