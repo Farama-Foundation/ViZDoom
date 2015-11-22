6ebfbd770e9745e102eb6e2a5e13a43b4b87036a
Marqt <marek@wydmuch.poznan.pl>
2015-11-22 20:08:38 +0100

    Vizia API compile.sh fix
    Former-commit-id: 5bb89cefa7138e572693d0e8a0d097e0de498696
    Former-commit-id: d94f3f24fe0dce470b637fc78427e4c621326c0e
    Former-commit-id: 0349e40eb52802b79cba40812529619ef7884d96

--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash	
-g++ -I. ViziaDoomController.cpp ViziaDoomControllerExample.cpp -o controllerExample -lrt -lpthread -L/usr/local/lib -lboost_system -lboost_thread -lSDL2
-g++ -I. ViziaDoomController.cpp ViziaMain.cpp ViziaDoomControllerExample.cpp -o mainExample -lrt -lpthread -L/usr/local/lib -lboost_system -lboost_thread
+g++ -I . ViziaDoomController.cpp ViziaDoomControllerExample.cpp -o controllerExample -lrt -lpthread -L/usr/local/lib -lboost_system -lboost_thread -lSDL2
+g++ -I . ViziaDoomController.cpp ViziaMain.cpp ViziaMainExample.cpp -o mainExample -lrt -lpthread -L/usr/local/lib -lboost_system -lboost_thread
