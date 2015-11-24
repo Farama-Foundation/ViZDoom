37595cd907f171557cfbf5f5854035957ad913ad
michał <kempka.michal@gmail.com>
2015-11-24 14:09:38 +0100

    Added Makefile to vizia_api_src.
    Former-commit-id: d087dd3f185755c17aa8125896e64dda400adbc3
    Former-commit-id: 67f9c787a589c4a034d484275cf574dc5153a09b
    Former-commit-id: e329d1403aead7e6aaef007d11be071507e0e29f

--- vizia_api_src/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash	
-g++ -I . ViziaDoomController.cpp ViziaDoomControllerExample.cpp -o controllerExample -lrt -lpthread -L/usr/local/lib -lboost_system -lboost_thread -lSDL2
-g++ -I . ViziaDoomController.cpp ViziaMain.cpp ViziaMainExample.cpp -o mainExample -lrt -lpthread -L/usr/local/lib -lboost_system -lboost_thread
--- vizia_api_src/compile_api.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export CPLUS_INCLUDE_PATH=/usr/include/python2.7 	
-g++ -c -fPIC PythonVizia.cpp -o vizia.o -I. -lrt -lpthread  -L/usr/local/lib -lboost_system -lboost_thread
-g++ -shared -Wl,-soname,api.so -o vizia.so  vizia.o -lpython2.7 -lboost_python -I. -lrt -lpthread  -L/usr/local/lib -lboost_system -lboost_thread
