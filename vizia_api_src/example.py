4c82c14618529103dcb551d38061f19a9dfd0dac
michał <kempka.michal@gmail.com>
2015-11-30 13:19:01 +0100

    The bug seems to persist.
    Former-commit-id: 0974f5df40ec9e1ca361fceb8505048aadbe9fad
    Former-commit-id: 45a5c56b32f87db5881a2b77951e3e681b384c31
    Former-commit-id: 149522095252630d64253ff8e4de55ec96c3f43a

--- vizia_api_src/example.py
+++ vizia_api_src/example.py
@@ -17,9 +17,9 @@ game.set_screen_resolution(320,0)
 
 game.set_doom_game_path("zdoom")
 game.set_doom_iwad_path("doom2.wad")
-game.set_doom_file_path("end_test.wad")
+game.set_doom_file_path("s1_b.wad")
 game.set_doom_map("map01")
-game.set_episode_timeout_in_doom_tics(200)
+game.set_episode_timeout_in_doom_tics(300)
 
 game.set_render_hud(False)
 game.set_render_crosshair(False)
@@ -43,7 +43,7 @@ right = actions[1]
 shoot = actions[2]
 idle = [False,False,False]
 
-iters = 1000
+iters = 1200
 sleep_time = 0.01
 #game.new_episode()
 start = time()
@@ -53,8 +53,9 @@ for i in range(iters):
 		print "new episode"
 		sleep(2)
 
+	print "action"
 	r = game.make_action(choice(actions))
-	
+
 	print "reward:",r
 	s = game.get_state()
 	#if r!=0:
