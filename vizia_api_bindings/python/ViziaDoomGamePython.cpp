a2da9a74fe404c48bf35a240196479eaa4e9c53d
michał <kempka.michal@gmail.com>
2015-12-09 23:56:30 +0100

    Just some minor code rearranging.
    Former-commit-id: 4acab16908ec11c27a7ab6e86e45fa7a6090b2df
    Former-commit-id: de82739ad7c0cf242ea3156121db1a2035cd9f80

--- vizia_api_bindings/python/ViziaDoomGamePython.cpp
+++ vizia_api_bindings/python/ViziaDoomGamePython.cpp
@@ -1,5 +1,9 @@
 #include "ViziaDoomGamePython.h"
 
+#include <iostream>
+using std::cout;
+using std::endl;
+
 namespace Vizia {
 
     using boost::python::tuple;
@@ -9,14 +13,17 @@ namespace Vizia {
 
     DoomGamePython::DoomGamePython() {
         import_array();
+        Py_Initialize();
+        /* Numpy arrays won't work unless this strnage function is envoked.*/
+        boost::python::numeric::array::set_module_and_type("numpy", "ndarray");
+        //boost::numpy::initialize();
     }
 
     bool DoomGamePython::init() {
         bool initSuccess = DoomGame::init();
         /* fill state format */
         if (initSuccess) {
-            /* Numpy arrays won't work unless this strnage function is envoked.*/
-            boost::python::numeric::array::set_module_and_type("numpy", "ndarray");
+            
 
             DoomGame::StateFormat cppFormat = DoomGame::getStateFormat();
             boost::python::list imageShape;
@@ -45,12 +52,10 @@ namespace Vizia {
             return PY_NONE;
         }
         DoomGame::State state = DoomGame::getState();
-        //TODO convert the image state to numpy array
         PyObject *img = PyArray_SimpleNewFromData(3, this->imageShape, NPY_UBYTE, state.imageBuffer);
         boost::python::handle<> handle(img);
         boost::python::numeric::array npyImg(handle);
-
-        //TODO copy or no?
+        //TODO copy or not?
         if (state.vars.size() > 0) {
 
             npy_intp varLen = boost::python::extract<int>(this->stateFormat[1]);
@@ -58,11 +63,11 @@ namespace Vizia {
             boost::python::handle<> handle(vars);
             boost::python::numeric::array npyVars(handle);
 
-            //TODO copy or no?
+            //TODO copy or not?
             return boost::python::make_tuple(state.number, npyImg.copy(), npyVars.copy());
         }
         else {
-            //TODO copy or no?
+            //TODO copy or not?
             return boost::python::make_tuple(state.number, npyImg.copy());
         }
 
