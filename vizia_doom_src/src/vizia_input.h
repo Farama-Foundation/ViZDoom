532bff22208f4fd2c4297ae82a02ce119699756f
Marqt <marek@wydmuch.poznan.pl>
2015-12-10 02:40:26 +0100

    Vizia input rewrite to work exactly like physical input, selected weapon game vars with real values, no custom cursor
    Former-commit-id: 33f5ff0395520a56a9c126fb39b393c31b1d7281
    Former-commit-id: fee7a5190f934a47d0bbc784710c013e085e86fe

--- vizia_doom_src/src/vizia_input.h
+++ vizia_doom_src/src/vizia_input.h
@@ -1,17 +1,13 @@
 #ifndef __VIZIA_INPUT_H__
 #define __VIZIA_INPUT_H__
 
-void Vizia_MouseEvent(int x, int y);
-
-void Vizia_ButtonEvent(int button, bool state, bool oldState);
+bool Vizia_HasCounterBT(int button);
 
 int Vizia_CounterBT(int button);
 
 char* Vizia_BTToCommand(int button, bool state);
 
 char* Vizia_GetCommandWithState(char* command, bool state);
-
-void Vizia_ButtonCommand(int button, bool state, bool oldState);
 
 void Vizia_Mouse(int x, int y);
 
