9e7ac73cf70dcfc2aa98f7692bd02d8ac2e3b13a
buszumpulum <buszumpulum@poczta.fm>
2016-01-15 18:51:45 +0100

    Vizia depth updated with lock mechanism (not updating depth value while locked). Sky and weapon depth fixed.
    Former-commit-id: 13d1a2c3154113622774f271f3a260d1d808fe7a
    Former-commit-id: ddce1d736907f61f73775b811983b8f17b0585d1

--- vizia_doom_src/src/vizia_depth.cpp
+++ vizia_doom_src/src/vizia_depth.cpp
@@ -31,42 +31,56 @@ depthBuffer::~depthBuffer() {
     surface = NULL;
 #endif //VIZIA_DEPTH_TEST
 }
-
+//get depth buffer pointer
 u_int8_t* depthBuffer::getBuffer() { return buffer; }
 
+//get pointer for requested pixel (x, y coords)
 u_int8_t* depthBuffer::getBufferPoint(unsigned int x, unsigned int y) {
     if( x < bufferWidth && y < bufferHeight )
         return buffer + x + y*bufferWidth;
     else
     return NULL;}
 
+//set point(x,y) value with depth stored in actualDepth
 void depthBuffer::setPoint(unsigned int x, unsigned int y) {
     u_int8_t *dpth = getBufferPoint(x, y);
     if(dpth!=NULL)
         *dpth = actualDepth;
 }
 
+//set point(x,y) value with requested depth
 void depthBuffer::setPoint(unsigned int x, unsigned int y, u_int8_t depth) {
     u_int8_t *dpth = getBufferPoint(x, y);
     if(dpth!=NULL)
         *dpth = depth;
 }
 
-void depthBuffer::setActualDepth(u_int8_t depth) {this->actualDepth=depth; }
+//store depth value for later usage
+void depthBuffer::setActualDepth(u_int8_t depth) {
+    if(this->isLocked())
+        return;
+    this->actualDepth=depth;
+}
 
+//store depth value for later usage with automated conversion based on stored boundries
 void depthBuffer::setActualDepthConv(int depth) {
+    if(this->isLocked())
+        return;
     if(depth>maxDepth)
         this->actualDepth=0;
     else if(depth<minDepth)
         this->actualDepth=255;
     else {
         depth-=minDepth;
-        this->actualDepth = 255 - (depth-minDepth) / this->convSteps;
+        this->actualDepth = (unsigned int) 255 - (depth-minDepth) / this->convSteps;
     }
 }
 
+//increase or decrease stored depth value by adsb
 void depthBuffer::updateActualDepth(int adsb) {
     int act = this->actualDepth;
+    if(this->isLocked())
+        return;
     if(act+adsb>255)
         this->actualDepth=255;
     else if(act+adsb<0)
@@ -74,45 +88,60 @@ void depthBuffer::updateActualDepth(int adsb) {
     else
         this->actualDepth+=adsb;
 }
-
+//store x value for later usage
 void depthBuffer::storeX(int x) {this->tX=x; }
 
+//store y value for later usage
 void depthBuffer::storeY(int y) {this->tY=y; }
 
+//get stored x value
 int depthBuffer::getX(void) {return this->tX; }
 
+//get stored y value
 int depthBuffer::getY(void) {return this->tY; }
 
-
+//get buffer size
 unsigned int depthBuffer::getBufferSize(){
     return this->bufferSize;
 }
 
+//set boundries for storing depth value with conversion
 void depthBuffer::setDepthBoundries(int maxDepth, int minDepth) {
     this->maxDepth=maxDepth;
     this->minDepth=minDepth;
     this->convSteps = (maxDepth-minDepth)/255;
 }
 
+//get buffer width
 unsigned int depthBuffer::getBufferWidth(){
     return bufferWidth;
 }
 
+//get buffer height
 unsigned int depthBuffer::getBufferHeight(){
     return bufferHeight;
 }
 
+//clear buffer - set every point to 0
 void depthBuffer::clearBuffer() {
     for(unsigned int i=0;i<bufferSize;i++)
         buffer[i]=0;
 }
 
+//set every point to color value
 void depthBuffer::clearBuffer(u_int8_t color) {
     for(unsigned int i=0;i<bufferSize;i++)
         buffer[i]=color;
 }
 
+void depthBuffer::lock() {this->locked=true; }
+
+void depthBuffer::unlock() {this->locked=false; }
+
+bool depthBuffer::isLocked() { return this->locked; }
+
 #ifdef VIZIA_DEPTH_TEST
+//update depth debug window
 void depthBuffer::Update() {
     SDL_Surface* surf = SDL_CreateRGBSurfaceFrom(this->buffer, this->bufferWidth, this->bufferHeight, 8,
                                                  this->bufferWidth, 0, 0, 0, 0);
