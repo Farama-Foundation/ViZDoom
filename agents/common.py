e8423d133e949dfc3680c353c0048ba8dbabe604
michał <kempka.michal@gmail.com>
2016-01-19 15:48:25 +0100

    qengine structural changes.
    Former-commit-id: baab5c8cbd5d48097f79c2ffc1ecffccd54ddcb4
    Former-commit-id: 254eb2d412133eff63eb71027ac152c7e6ba4f8c

--- agents/common.py
+++ agents/common.py
@@ -16,6 +16,16 @@ from lasagne.layers import get_all_param_values
 
 import cv2
 
+def sec_to_str(sec):
+    res = str(int(sec%60)) +"s"
+    sec = int(sec/60)
+    if sec>0:
+        res = str(int(sec%60)) +"m "+res
+        sec = int(sec/60)
+        if sec>0:
+            res = str(int(sec%60)) +"h " +res
+            sec = int(sec/60)
+    return res
 # Common functions for learn.py and watch.py 
 def agenerator_left_right_move(the_game):
     idle = [0,0,0]
@@ -24,7 +34,7 @@ def agenerator_left_right_move(the_game):
     move = [0,0,1]
     move_left = [1,0,1]
     move_right = [0,1,1]
-    return [left, right, move]
+    return [idle,left, right, move]
 
 def create_cnn_evaluator(state_format, actions_number, batch_size, gamma):
     cnn_args = dict()
@@ -35,10 +45,10 @@ def create_cnn_evaluator(state_format, actions_number, batch_size, gamma):
     cnn_args["updates"] = lasagne.updates.nesterov_momentum
     #cnn_args["learning_rate"] = 0.01
 
-    network_args = dict(hidden_units=[1200], hidden_layers=1)
-    network_args["conv_layers"] = 3
+    network_args = dict(hidden_units=[800], hidden_layers=1)
+    network_args["conv_layers"] = 2
     network_args["pool_size"] = [(2, 2),(2,2),(1,2)]
-    network_args["num_filters"] = [48,48,48]
+    network_args["num_filters"] = [32,32,48]
     network_args["filter_size"] = [7,4,2]
     #network_args["hidden_nonlin"] = None
 
--- agents/evaluators.py
+++ agents/evaluators.py
@@ -140,18 +140,15 @@ class MLPEvaluator:
             loss = self._learn(self._input_image_buffer, target)
         self._loss_history.append(loss)
 
-    def best_action(self, state, verbose = False):
+    def best_action(self, state):
         if self._misc_state_included:
             qvals = self._evaluate(state[0].reshape(self._image_input_shape), state[1].reshape(self._misc_input_shape))
             a = np.argmax(qvals)
         else:
             qvals = self._evaluate(state[0].reshape(self._image_input_shape))
             a = np.argmax(qvals)
-        
-        if verbose:
-            print qvals 
         return a
-
+        
     def get_mean_loss(self, clear = True):
         m = np.mean(self._loss_history)
         self._loss_history = []
