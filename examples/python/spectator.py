38b28aeb9f7f9fcc91a3f7fea4be5d124e7e18cd
michał <kempka.michal@gmail.com>
2016-01-20 23:42:59 +0100

    Scenario images are now in tex file.
    Former-commit-id: f1851d57df52b1f4413c2a914e39b163cef55915
    Former-commit-id: e58c58d52f51685e185b602feed5c5377405cfb6

--- examples/python/spectator.py
+++ examples/python/spectator.py
@@ -24,6 +24,7 @@ game.load_config("config_basic.properties")
 #game.load_config("config_health_gathering.properties")
 #game.load_config("config_my_way_home.properties")
 #game.load_config("config_predict_position.properties")
+#game.load_config("config_take_cover.properties")
 
 game.set_screen_resolution(ScreenResolution.RES_640X480)
 
