f475344709ab7b201914d2dd21f030253368d83c
michał <kempka.michal@gmail.com>
2016-01-24 00:32:55 +0100

    Started createing more useful multiplayer python examples.
    Former-commit-id: 15e6e96566170e397ebacc1ba09ed5bb63e0dc04
    Former-commit-id: 11918ee1f918828599fb5c2b0983ad1ff7a308aa
    Former-commit-id: 8541e087723944aa09e931c715f7069cd9266792

---
+++ examples/python/host_multiplayer.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+
+from vizia import *
+from random import choice
+from time import sleep
+from time import time
+
+game = DoomGame()
+game.load_config("config_multi.properties")
+
+game.add_custom_game_arg("-host")
+game.add_custom_game_arg("2")
+game.add_custom_game_arg("-deathmatch")
+game.add_custom_game_arg("-warp")
+game.add_custom_game_arg("-01")
+game.set_mode(Mode.ASYNC_SPECTATOR)
+
+game.init()
+
+	
+while not game.is_episode_finished():	
+	s = game.get_state()
+	img = s.image_buffer
+	misc = s.game_variables
+
+	game.advance_action()
+	a = game.get_last_action()
+	r = game.get_last_reward()
+		
+	print "state #"+str(s.number)
+	print "game variables: ", misc
+	print "action:", a
+	print "reward:",r
+	print "====================="
+		
+	
+print "episode finished!"
+print "summary reward:", game.get_summary_reward()
+print "************************"
+
+game.close()
