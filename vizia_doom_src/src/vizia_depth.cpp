fcdcfc0fe60ec205f4fee3256bb20a43ec67e33d
buszumpulum <buszumpulum@poczta.fm>
2016-01-24 18:28:19 +0100

    vizia_no_console is now checked in main().
    Former-commit-id: db00f1718c54677156783e337e40556052b4337d
    Former-commit-id: 33a13a95f401066db712032547da9399b91bec00

--- vizia_doom_src/src/vizia_depth.cpp
+++ vizia_doom_src/src/vizia_depth.cpp
@@ -15,7 +15,17 @@ depthBuffer::depthBuffer(unsigned int width, unsigned int height) : bufferSize(h
 #ifdef VIZIA_DEPTH_TEST
     for(int j = 0; j < 256; j++)
     {
-        colors[j].r = colors[j].g = colors[j].b = j;
+        //colors[j].r = colors[j].g = colors[j].b = j;
+        colors[j].r= j%3==0 ? 255 : 0;
+        colors[j].g= j%3==1 ? 255 : 0;
+        colors[j].b= j%3==2 ? 255 : 0;
+        if(j%2==0)
+            colors[j].r=colors[j].g=colors[j].b = j;
+        if(j%7==0) {
+            colors[j].r = 100;
+            colors[j].g = 200;
+            colors[j].b = 255;
+        }
     }
     this->window = SDL_CreateWindow("Vizia Depth Buffer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width,
                                     height, SDL_WINDOW_SHOWN);
