2bc2d29eababb29013bc2d3d8106b44d1bc4012b
michał <kempka.michal@gmail.com>
2016-01-27 23:54:09 +0100

    Multiplayer example is nicer.
    Former-commit-id: 1f46425b8160b1555d804ed439b56a083f249ef5
    Former-commit-id: 0a82db41eec39956f267173236637977310080ee
    Former-commit-id: 3c24b588f1725b873c24c9f95aafa1d6fd1cb5b8

--- examples/python/multiplayer.py
+++ examples/python/multiplayer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 #use ./viziazdoom -host 2 -deathmatch -warp 01 to start game host
-#or run host_multiplayer.py
+#or run host_multiplayer.py to have a host
 
 from __future__ import print_function
 from vizia import *
@@ -17,34 +17,34 @@ game.load_config("config_multi.properties")
 game.set_mode(Mode.ASYNC_PLAYER)
 game.add_custom_game_arg("-join")
 game.add_custom_game_arg("127.0.0.1")
+game.set_window_visible(False)
 game.init()
 
 
-actions = [[1,0,0,0],[0,1,0,0],[0,0,0,1]]
+actions = [[1,0,0,0,0,0,0,0,0],[0,1,0,0,0,0,0,0,0],[0,0,1,0,0,0,0,0,0]]
 
-iters = 10000
-sleep_time = 0
+episodes = 1
 
-for i in range(iters):
+for i in range(episodes):
+	
+	while not game.is_episode_finished():	
+		s = game.get_state()
+		img = s.image_buffer
+		misc = s.game_variables
 
-	if game.is_episode_finished():
-		print("episode finished!")
-		print("summary reward:", game.get_summary_reward())
-		print("************************")
-		exit(0)
-
-	s = game.get_state()
-
-	print("gametic:", game.get_episode_time())
-	print("state:", s.number)
-
-	if sleep_time>0:
-		sleep(sleep_time)
-
-	r = game.make_action(choice(actions))
-
-	print("reward:", r)
-	print("=====================")
+		game.make_action(choice(actions))
+		a = game.get_last_action()
+		r = game.get_last_reward()
+			
+		print("state #"+str(s.number))
+		print("game variables: ", misc)
+		print("action:", a)
+		print("reward:", r)
+		print("=====================")
+	
+	print("episode finished!")
+	print("summary reward:", game.get_summary_reward())
+	print("************************")
 
 game.close()
 
