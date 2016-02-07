e1b899e624caa9835cda82110695da9d2d41e346
LowcaPromocji <jatokuba@gmail.com>
2016-02-07 20:34:52 +0100

    Java fixed + 2 functions changed
    Former-commit-id: 64c9567379e1759bdcaa16bfabf093f25d2b3859
    Former-commit-id: 7c1207544039a989b03bad502aebe9aca12fa31d [formerly c2c1422cfbbd36b4618e1e4a79bec14185a55664] [formerly 8e0e696c94c6df55fe1cfc6bd863d0e01b7c5b4f [formerly b05d00a789314685489227c7e5b3cb00bcc3af6f]]
    Former-commit-id: 6ea499b6663ae2a14c49db3837ec994a912633d2 [formerly 10d94f2dac0d07c1ea769c73c130af31fca0ea00]
    Former-commit-id: 1848d1ae80a465973f60840dd02d20665327b683

--- vizia_api_bindings/java/enums/ScreenFormat.java
+++ vizia_api_bindings/java/enums/ScreenFormat.java
@@ -1,17 +1,17 @@
 package enums;
 public enum ScreenFormat {
         CRCGCB,
-        CRCGCBZB,
+        CRCGCBDB,
         RGB24,
         RGBA32,
         ARGB32,
         CBCGCR,
-        CBCGCRZB,
+        CBCGCRDB,
         BGR24,
         BGRA32,
         ABGR32,
         GRAY8,
-        ZBUFFER8,
-        DOOM_256_COLORS;
+        DEPTH_BUFFER8,
+        DOOM_256_COLORS8;
 
     }
