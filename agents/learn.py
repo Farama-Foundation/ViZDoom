779e0e9e10dd326ba1f40692804300b331d47751
michał <kempka.michal@gmail.com>
2016-02-15 16:25:48 +0100

    Health gathering learnt successfully.
    Former-commit-id: c171778c51b559d8baef308fa1265d0a57a2d46e
    Former-commit-id: 135a83ec193391922b8dac298d6aac0e1f6438f7 [formerly 9b8495624c2cb03c5505af448c0d073b08fa631a] [formerly 3a641e77ca3cff218d5088b085d226f2091a7ff7 [formerly 6dc730022014486a84eccf02b67861ada08448e0]]
    Former-commit-id: 3ba58f977280e26b732076e1a8684bc39071efc5 [formerly 8accf4513c93ac655fac22f2c64e86a0b98ec166]
    Former-commit-id: 6737c0b95cecb050708f83638753ccd9cbf78dd2

--- agents/learn.py
+++ agents/learn.py
@@ -2,41 +2,52 @@
 from common import *
 from vizia import *
 from tqdm import tqdm
+import pickle
 
-suffix = "xxx"
-skiprate = 1
-np.set_printoptions(precision=4, suppress = True)
+filename = "health_guided"
+setup = engine_setup_health
+config_file = "config_health_guided" + ".properties"
+
+suffix = ""
+skiprate = 4
 savefile = None
-savefile = "params/exp_skip"+str(skiprate) + suffix
-loadfile = None
+savefile = "params/"+filename+"_skip"+str(skiprate) + suffix
+loadfile = "params/"+filename+"_skip"+str(skiprate) + suffix
 
-results_savefile = "results/raw/res_exp_skip"+str(skiprate) + suffix
-
+results_savefile = "results/health_47"
+if results_savefile:
+    results = dict()
+    results["epoch"] = []
+    results["time"] = []
+    results["overall_time"] = []
+    results["mean"] = []
+    results["std"] = []
+    results["max"] = []
+    results["min"] = []
+    results["epsilon"] = []
 game = DoomGame()
 game.load_config("config_common.properties")
-#game.load_config("config_health_guided.properties")
-game.load_config("config_basic.properties")
+game.load_config(config_file)
 
 
 #game.clear_available_game_variables()
 print "Initializing DOOM ..."
 game.init()
-print "\nDOOM initialized."
+print "DOOM initialized."
 
 
 if loadfile:
     engine = QEngine.load(game, loadfile)
-    engine._epsilon = 0.1
-
+    engine._epsilon = 0.4
+    engine._epsilon_decay_start = 0
+    engine._epsilon_decay_stride = 0.4/1000000.0
 else:
-    engine_args = engine_setup_basic(game)
+    engine_args = setup(game)
     engine_args["skiprate"] = skiprate
     engine = QEngine(**engine_args)
 
-if results_savefile:
-    res_f = open(results_savefile, 'w')
-    res_f.write(engine.params_to_print())
-    res_f.write("Epoch | time | overall_time| steps | mean result| results\n")
+
+
 print "\nNetwork architecture:"
 for p in get_all_param_values(engine.get_network()):
 	print p.shape
@@ -44,37 +55,43 @@ for p in get_all_param_values(engine.get_network()):
 
 
 epochs = np.inf
-training_steps_per_epoch = 5000
+training_steps_per_epoch = 10000
 test_episodes_per_epoch = 100
 test_frequency = 1;
 overall_start = time()
 
 
-epoch = 0
+epoch = 47
 print "\nLearning ..."
 while epoch < epochs:
     print "\nEpoch", epoch
-    
+    train_time = 0
     if training_steps_per_epoch>0:
         rewards = []
         start = time()
 
-        engine.new_episode(True)
+        engine.new_episode(update_state=True)
         for step in tqdm(range(training_steps_per_epoch)):
             if game.is_episode_finished():
                 r = game.get_summary_reward()
                 rewards.append(r)
-                engine.new_episode(True)
+                engine.new_episode(update_state=True)
             engine.make_learning_step()
         end = time()
         
-        train_time = sec_to_str(end - start)
+        train_time = end - start
+        
         print "Train:"
         print engine.get_actions_stats(True)
+
         mean_loss = engine._evaluator.get_mean_loss()
-        print "steps:", engine.get_steps(), ", mean:", np.mean(rewards), ", max:", np.max(
-            rewards), "min:", np.min(rewards), "mean_loss:",mean_loss, "eps:", engine.get_epsilon()
-        print "t:", train_time
+
+       	if len(rewards) == 0:
+       		rewards.append(-123)
+        rewards = np.array(rewards)
+
+        print "mean:", rewards.mean(),"std:", rewards.std(), "max:", rewards.max(), "min:", rewards.min(), "mean_loss:",mean_loss, "eps:", engine.get_epsilon()
+        print "t:", sec_to_str(train_time)
             
     # learning mode off
     if (epoch+1) % test_frequency == 0 and test_episodes_per_epoch > 0:
@@ -89,27 +106,35 @@ while epoch < epochs:
         
         print "Test"
         print engine.get_actions_stats(clear=True, norm=False)
-        m = np.mean(rewards)
-        print "steps:", engine.get_steps(), ", mean:", m, "max:", np.max(rewards), "min:", np.min(rewards)
+        rewards = np.array(rewards)
+        print "mean:", rewards.mean(),"std:", rewards.std(), "max:", rewards.max(), "min:", rewards.min()
         print "t:", sec_to_str(end - start)
     
     overall_end = time()
-    overall_time = sec_to_str(overall_end - overall_start)
+    overall_time = overall_end - overall_start
+    
     if results_savefile:
-        res_f.write( str(epoch)+"| " + train_time + "| " +overall_time +"| "+ str(engine.get_steps()) + "| " +str(m) +"| ")
-        for r in rewards:
-            res_f.write(str(r) +" ")
-        res_f.write("\n")
+        results["epoch"].append(epoch)
+        results["time"].append(train_time)
+        results["overall_time"].append(overall_time)
+        results["mean"].append(rewards.mean())
+        results["std"].append(rewards.std())
+        results["max"].append(rewards.max())
+        results["min"].append(rewards.min())
+        results["epsilon"].append(engine.get_epsilon())
 
-    epoch += 1
-
+        res_f = open(results_savefile, 'w')
+        pickle.dump(results,res_f)
+        res_f.close()
+	
+    epoch +=1
     print ""
 
     if savefile:
         engine.save(savefile)
 
     
-    print "Elapsed time:", overall_time
+    print "Elapsed time:", sec_to_str(overall_time)
     print "========================="
 
 
