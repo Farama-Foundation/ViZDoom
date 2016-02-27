913a74fd4908b2f81c728b7e2a54a72079230a94
LowcaPromocji <jatokuba@gmail.com>
2016-02-27 23:39:49 +0100

    Java fix + Seed example added

--- vizia_api_bindings/java/State.java
+++ vizia_api_bindings/java/State.java
@@ -3,16 +3,16 @@ import java.util.List;
 
 public class State{
 	public int number;
-	public int[] vars;
+	public int[] gameVariables;
 	public int[] imageBuffer;
 	public State(){
 		this.number=-1;
-		this.vars=null;
+		this.gameVariables=null;
 		this.imageBuffer=null;
 	};
 	public State(int number, int[] vars , int[] imageBuffer){
 		this.number=number;
-		this.vars=vars;
+		this.gameVariables=vars;
 		this.imageBuffer=imageBuffer;
 	}
 }
