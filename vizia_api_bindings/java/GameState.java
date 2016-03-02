13545f9fd78e1cd2245314313ed6b0da962a0dcd
LowcaPromocji <jatokuba@gmail.com>
2016-03-02 10:10:26 +0100

    Java update
    Former-commit-id: cea1d3f4fdf1e90d8e8f0df54aecf4b899f69b9c

--- vizia_api_bindings/java/GameState.java
+++ vizia_api_bindings/java/GameState.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class State{
+public class GameState{
 	public int number;
 	public int[] gameVariables;
 	public int[] imageBuffer;
-	public State(){
+	public GameState(){
 		this.number=-1;
 		this.gameVariables=null;
 		this.imageBuffer=null;
 	};
-	public State(int number, int[] vars , int[] imageBuffer){
+	public GameState(int number, int[] vars , int[] imageBuffer){
 		this.number=number;
 		this.gameVariables=vars;
 		this.imageBuffer=imageBuffer;
