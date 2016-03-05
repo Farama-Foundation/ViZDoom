ccf2caae812230768ee7f59a40542066ea919180
michał <kempka.michal@gmail.com>
2016-03-05 13:50:59 +0100

    Corrected python examples after recent updates.
    Former-commit-id: 10ec3b30364beb86dfe7a095534efe7b8223cfa0

--- examples/python/cig_host.py
+++ examples/python/cig_host.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 from vizdoom import *
+from random import choice
 
 game = DoomGame()
 
@@ -12,21 +13,21 @@ game.load_config("../config/cig.cfg")
 game.set_doom_game_path("../../scenarios/freedoom2.wad")
 # game.set_doom_game_path("../../scenarios/doom2.wad")  # Not provided with environment due to licences
 
-game.set_doom_map("map01")      # Limited deathmatch.
-# game.set_doom_map("map02")    # Full deathmatch.
+game.set_doom_map("map01")	  # Limited deathmatch.
+# game.set_doom_map("map02")	# Full deathmatch.
 
 #Host game with options that will be used in the competition.
 game.add_game_args("-host 8 "   # This machine will function as a host for a multiplayer game with this many players (including this machine). It will wait for other machines to connect using the -join parameter and then start the game when everyone is connected.
-"-deathmatch "                  # Deathmatch rules are used for the game.
-"+timelimit 10.0 "              # The game (episode) will end after this many minutes have elapsed.
-"+sv_forcerespawn 1 "           # Players will respawn automatically after they die.
-"+sv_noautoaim 1 "              # Autoaim is disabled for all players.
-"+sv_respawnprotect 1 "         # Players will be invulnerable for two second after spawning.
-"+sv_spawnfarthest 1 "          # Players will be spawned as far as possible from any other players.
-"+vizdoom_nocheat 1");            # Disables depth buffer and the ability to use commands that could interfere with multiplayer game.
+"-deathmatch "				  # Deathmatch rules are used for the game.
+"+timelimit 10.0 "			  # The game (episode) will end after this many minutes have elapsed.
+"+sv_forcerespawn 1 "		   # Players will respawn automatically after they die.
+"+sv_noautoaim 1 "			  # Autoaim is disabled for all players.
+"+sv_respawnprotect 1 "		 # Players will be invulnerable for two second after spawning.
+"+sv_spawnfarthest 1 "		  # Players will be spawned as far as possible from any other players.
+"+vizdoom_nocheat 1");			# Disables depth buffer and the ability to use commands that could interfere with multiplayer game.
 
 # Name Your AI.
-game.addGameArgs("+name AI");
+game.add_game_args("+name AI");
 
 # Multiplayer requires the use of asynchronous modes.
 game.set_mode(Mode.ASYNC_PLAYER)
@@ -49,12 +50,12 @@ while not game.is_episode_finished():
 	# game.advance_action();
 	# continue;
 
-    s = game.get_state()
-    # Analyze the state.
+	s = game.get_state()
+	# Analyze the state.
 
-    game.make_action(choice(actions))
-    # Make your action.
+	game.make_action(choice(actions))
+	# Make your action.
 
-    print("Frags:", game.get_game_variable(GameVariable.FRAGCOUNT))
+	print("Frags:", game.get_game_variable(GameVariable.FRAGCOUNT))
 
 game.close()
