99f34e33ec3a3b323cf0abb312c9ba44f64b816c
Marqt <marek@wydmuch.poznan.pl>
2016-05-28 21:16:03 +0200

    Imprved multiple_instances example

--- examples/python/multiple_instances.py
+++ examples/python/multiple_instances.py
@@ -3,13 +3,22 @@
 from __future__ import print_function
 from vizdoom import *
 from random import choice
+
+# For multiplayer game use process (ZDoom's multiplayer sync mechanism prevents threads to work as expected).
 from multiprocessing import Process
+
+# For singleplayer games threads can also be used.
+#from threading import Thread
 
 def player1():
     game = DoomGame()
+
+    #game.load_config('../config/basic.cfg')
+    # or
     game.load_config('../config/multi_duel.cfg')
-    game.add_game_args("-host 2 -deathmatch +timelimit 1.0 +sv_forcerespawn 1 +sv_respawnprotect 1 +sv_spawnfarthest 1")
-    game.add_game_args("+name PLAYER1")
+    game.add_game_args("-host 2 -deathmatch +timelimit 1.0 +sv_spawnfarthest 1")
+    game.add_game_args("+name Player1")
+
     game.init()
 
     actions = [[True,False,False],[False,True,False],[False,False,True]]
@@ -25,9 +34,13 @@ def player1():
 
 def player2():
     game = DoomGame()
+
+    #game.load_config('../config/basic.cfg')
+    # or
     game.load_config('../config/multi_duel.cfg')
     game.add_game_args("-join 127.0.0.1")
-    game.add_game_args("+name PLAYER2")
+    game.add_game_args("+name Player2")
+
     game.init()
 
     actions = [[True,False,False],[False,True,False],[False,False,True]]
@@ -41,8 +54,10 @@ def player2():
 
     game.close()
 
+#p1 = Thread(target = player1)
+#p1.start()
 
-p1 = Process(target=player1)
+p1 = Process(target = player1)
 p1.start()
 
 player2()
