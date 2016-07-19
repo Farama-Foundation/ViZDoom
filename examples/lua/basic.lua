e3c2034c07db404c9cb9c4678f1614b868ee142f
Marqt <marek@wydmuch.poznan.pl>
2016-07-19 02:25:37 +0200

    basic.lua, scenarios.lua, spectator.lua examples

--- examples/lua/basic.lua
+++ examples/lua/basic.lua
@@ -1,68 +1,146 @@
-game = DoomGame()
+#!/usr/bin/env lua
 
+----------------------------------------------------------------------
+-- This script presents how to use the most basic features of the environment.
+-- It configures the engine, and makes the agent perform random actions.
+-- It also gets current state and reward earned with the action.
+-- <episodes> number of episodes are played.
+-- Random combination of buttons is chosen for every action.
+-- Game variables from state and last reward are printed.
+--
+-- To see the scenario description go to "../../scenarios/README.md"
+----------------------------------------------------------------------
+
+require("vizdoom")
+
+-- Create DoomGame instance. It will run the game and communicate with you.
+game = vizdoom.DoomGame()
+
+-- Now it's time for configuration!
+-- load_config could be used to load configuration instead of doing it here with code.
+-- If load_config is used in-code configuration will work. Note that the most recent changes will add to previous ones.
+-- game.load_config("../../examples/config/basic.cfg")
+
+-- Sets path to vizdoom engine executive which will be spawned as a separate process. Default is "./vizdoom".
 game:set_vizdoom_path("../../bin/vizdoom")
-game:set_doom_game_path("../../scenarios/freedoom2.wad")
---game.set_doom_game_path("../../scenarios/doom2.wad")
-game:set_doom_scenario_path("../../scenarios/basic.wad")
---game:set_doom_scenario_path("../../scenarios/deadly_corridor.wad")
-game:set_doom_map("map01")
-game:set_screen_resolution(DoomGame.RES_640X480)
 
+-- Sets path to doom2 iwad resource file which contains the actual doom game. Default is "./doom2.wad".
+game:set_doom_game_path("../../scenarios/freedoom2.wad")
+--game.set_doom_game_path("../../scenarios/doom2.wad") -- Not provided with environment due to licences.
+
+-- Sets path to additional resources wad file which is basically your scenario wad.
+-- If not specified default maps will be used and it's pretty much useless... unless you want to play good old Doom.
+game:set_doom_scenario_path("../../scenarios/basic.wad")
+
+-- Sets map to start (scenario .wad files can contain many maps).
+game:set_doom_map("map01")
+
+-- Sets resolution. Default is 320X240
+game:set_screen_resolution(vizdoom.ScreenResolution.RES_640X480)
+
+-- Sets the screen buffer format. Not used here but now you can change it. Defalut is CRCGCB.
+game:set_screen_format(vizdoom.ScreenFormat.RGB24)
+
+-- Enables depth buffer.
+game:set_depth_buffer_enabled(true)
+
+-- Enables labeling of in game objects labeling.
+game:set_labels_buffer_enabled(true)
+
+-- Enables buffer with top down map of he current episode/level .
+game:set_map_buffer_enabled(true)
+
+-- Sets other rendering options
 game:set_render_hud(false)
 game:set_render_crosshair(false)
 game:set_render_weapon(true)
 game:set_render_decals(false)
 game:set_render_particles(false)
 
+-- Adds buttons that will be allowed.
+game:add_available_button(vizdoom.Button.MOVE_LEFT)
+game:add_available_button(vizdoom.Button.MOVE_RIGHT)
+game:add_available_button(vizdoom.Button.ATTACK)
 
-game:add_available_button(DoomGame.MOVE_LEFT)
-game:add_available_button(DoomGame.MOVE_RIGHT)
-game:add_available_button(DoomGame.ATTACK)
+-- Adds game variables that will be included in state.
+game:add_available_game_variable(vizdoom.GameVariable.AMMO2)
 
-game:add_available_game_variable(DoomGame.AMMO2)
+-- Causes episodes to finish after 200 tics (actions)
+game:set_episode_timeout(200)
 
-
-game:set_episode_timeout(100)
+-- Makes episodes start after 10 tics (~after raising the weapon)
 game:set_episode_start_time(10)
-game:set_window_visible(true)
-game:set_sound_enabled(true)
-game:set_living_reward(-1)
-game:set_mode(DoomGame.PLAYER)
 
+-- Makes the window appear (turned on by default)
+game:set_window_visible(true)
+
+-- Turns on the sound. (turned off by default)
+game:set_sound_enabled(true)
+
+-- Sets the livin reward (for each move) to -1
+game:set_living_reward(-1)
+
+-- Sets ViZDoom mode (PLAYER, ASYNC_PLAYER, SPECTATOR, ASYNC_SPECTATOR, PLAYER mode is default)
+game:set_mode(vizdoom.Mode.PLAYER)
+
+-- Initialize the game. Further configuration won't take any effect from now on.
 game:init()
 
-
+-- Define some actions. Each list entry corresponds to declared buttons:
+-- MOVE_LEFT, MOVE_RIGHT, ATTACK
+-- 5 more combinations are naturally possible but only 3 are included for transparency when watching.
 actions = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}
 
+-- Run this many episodes
 episodes = 10
 
+-- Sets time that will pause the engine after each action.
+-- Without this everything would go too fast for you to keep track of what's happening.
 sleep_time = 28
 
 for i = 1, episodes do
 
     print("Episode #" .. i)
 
+    -- Starts a new episode. It is not needed right after init() but it doesn't cost much. At least the loop is nicer.
     game:new_episode()
-
     while not game:is_episode_finished() do
 
+        -- Gets the state
         s = game:get_state()
+
+        -- Which consists of:
+        n           = s.number
+        v           = s.game_variables
+        screen_buf  = s.screen_buffer
+        depth_buf   = s.depth_buffer
+        labels_buf  = s.labels_buffer
+        map_buf     = s.map_buffer
+        labels      = s.labels
+
+        --Makes a random action and get remember reward.
         r = game:make_action(actions[math.random(1,3)])
 
-        screen = s.screen_buffer
-        depth = s.depth_buffer
-        labels = s.labels_buffer
-        map = s.map_buffer
+        --Makes a "prolonged" action and skip frames:
+        --skiprate = 4
+        --r = game.make_action(choice(actions), skiprate)
 
-        print("State # " .. s.number)
+        --The same could be achieved with:
+        --game.set_action(choice(actions))
+        --game.advance_action(skiprate)
+        --r = game.get_last_reward()
+
+        -- Prints state's reward.
+        print("State # " .. n)
         print("Reward: " .. r)
         print("=====================")
 
         if sleep_time > 0 then
-            sleep(sleep_time)
+            vizdoom.sleep(sleep_time)
         end
     end
 
+    -- Check how the episode went.
     print("Episode finished.")
     print("total reward: " .. game:get_total_reward())
     print("************************")
