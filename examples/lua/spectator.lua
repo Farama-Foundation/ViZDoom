e689a3feb8750d5de109c7b162351214bb468fe4
Marqt <marek@wydmuch.poznan.pl>
2016-07-19 02:25:37 +0200

    basic.lua, scenarios.lua, spectator.lua examples
    Former-commit-id: e3c2034c07db404c9cb9c4678f1614b868ee142f

--- examples/lua/spectator.lua
+++ examples/lua/spectator.lua
@@ -1,11 +1,9 @@
 #!/usr/bin/env lua
 
 ----------------------------------------------------------------------
--- This script presents how to run some scenarios.
+-- This script presents SPECTATOR mode. In SPECTATOR mode you play and
+-- your agent can learn from it.
 -- Configuration is loaded from "../../examples/config/<SCENARIO_NAME>.cfg" file.
--- <episodes> number of episodes are played.
--- Random combination of buttons is chosen for every action.
--- Game variables from state and last reward are printed.
 --
 -- To see the scenario description go to "../../scenarios/README.md"
 ----------------------------------------------------------------------
@@ -18,9 +16,9 @@ game = vizdoom.DoomGame()
 -- Don't load two configs cause the second will overrite the first one.
 -- Multiple config files are ok but combining these ones doesn't make much sense.
 
-game:load_config("../../examples/config/basic.cfg")
+--game:load_config("../../examples/config/basic.cfg")
 --game:load_config("../../examples/config/deadly_corridor.cfg")
---game:load_config("../../examples/config/deathmatch.cfg")
+game:load_config("../../examples/config/deathmatch.cfg")
 --game:load_config("../../examples/config/defend_the_center.cfg")
 --game:load_config("../../examples/config/defend_the_line.cfg")
 --game:load_config("../../examples/config/health_gathering.cfg")
@@ -28,47 +26,37 @@ game:load_config("../../examples/config/basic.cfg")
 --game:load_config("../../examples/config/predict_position.cfg")
 --game:load_config("../../examples/config/take_cover.cfg")
 
--- Makes the screen bigger to see more details.
+-- Enables freelook in engine
+game:add_game_args("+freelook 1")
+
 game:set_screen_resolution(vizdoom.ScreenResolution.RES_640X480)
+
+-- Enables spectator mode, so you can play. Sounds strange but it is agent who is supposed to watch not you.
+game:set_window_visible(true)
+game:set_mode(vizdoom.Mode.SPECTATOR)
+
 game:init()
 
--- Creates all possible actions depending on how many buttons there are.
-actions_num = game:get_available_buttons_size()
-actions = {}
-for i = 1, actions_num do
-    actions[i] = {}
-    for j = 1, actions_num do
-        actions[i][j] = 0
-        if i == j then actions[i][j] = 1 end
-    end
-end
-
-
 episodes = 10
-sleep_time = 28
 
 for i = 1, episodes do
 
     print("Episode #" .. i)
 
-    -- Not needed for the first episode but the loop is nicer.
     game:new_episode()
+
     while not game:is_episode_finished() do
 
-        -- Gets the state and possibly to something with it
         s = game:get_state()
 
-        -- Makes a random action and save the reward.
-        r = game:make_action(actions[math.random(1,actions_num)])
+        game:advance_action()
+        -- a = game:get_last_action()
+        r = game:get_last_reward()
 
         print("State # " .. s.number)
         print("Reward: " .. r)
         print("=====================")
 
-        -- Sleep some time because processing is too fast to watch.
-        if sleep_time > 0 then
-            vizdoom.sleep(sleep_time)
-        end
     end
 
     print("Episode finished.")
@@ -78,4 +66,3 @@ for i = 1, episodes do
 end
 
 game:close()
-
