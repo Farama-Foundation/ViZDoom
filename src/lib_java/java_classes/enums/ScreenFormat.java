3ae165c37f017a052ae6a63e427508b4fac904aa
michał <kempka.michal@gmail.com>
2016-08-21 13:02:29 +0200

    Java: Updated enums.
    Former-commit-id: f929dae8113539388f16d1342d9f46769a6f1016

--- src/lib_java/java_classes/enums/ScreenFormat.java
+++ src/lib_java/java_classes/enums/ScreenFormat.java
@@ -1,16 +1,13 @@
 package vizdoom;
 public enum ScreenFormat {
     CRCGCB,
-    CRCGCBDB,
     RGB24,
     RGBA32,
     ARGB32,
     CBCGCR,
-    CBCGCRDB,
     BGR24,
     BGRA32,
     ABGR32,
     GRAY8,
-    DEPTH_BUFFER8,
     DOOM_256_COLORS8;
 }
