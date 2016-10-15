187585bdf65edd9ac1aac7e3bd754d400697b8c6
michał <kempka.michal@gmail.com>
2016-10-16 01:12:20 +0200

    Added script for pip package assembly.

---
+++ scripts/assemble_python_wrapper.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+PYTHON_VERSION=$1
+
+if [ $# -ne 1 ];then
+    echo "Exactly one argument required: python version = {2,3}. Aborting."
+    exit 1
+fi
+if [ $1 -ne 2 ] && [ $1 -ne 3 ];then
+    echo "Python version should be '2' or '3'. Aborting."
+    exit 2
+fi
+
+PACKAGE_DESTINATION_DIRECTORY="./bin/python${PYTHON_VERSION}"
+PACKAGE_DESTINATION_PATH="${PACKAGE_DESTINATION_DIRECTORY}/vizdoom"
+WRAPPER_SOURCE="./src/lib_python/wrapper"
+VIZDOOM_PATH="./bin/vizdoom"
+PK3_PATH="./bin/vizdoom.pk3"
+PYTHON_BIN_PATH="${PACKAGE_DESTINATION_DIRECTORY}/vizdoom.so"
+FREEDOOM_PATH="./bin/freedoom2.wad"
+
+rm -rf ${PACKAGE_DESTINATION_PATH}
+cp -r ${WRAPPER_SOURCE} ${PACKAGE_DESTINATION_PATH}
+cp ${VIZDOOM_PATH} ${PACKAGE_DESTINATION_PATH}
+mv ${PYTHON_BIN_PATH} ${PACKAGE_DESTINATION_PATH}
+cp ${FREEDOOM_PATH} ${PACKAGE_DESTINATION_PATH}
+cp ${PK3_PATH} ${PACKAGE_DESTINATION_PATH}
