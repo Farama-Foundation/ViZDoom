d7c1ba5f9a118ad51be6701d04d0631957997e9c
Eugenio <culurciello@gmail.com>
2016-12-09 19:00:07 -0500

    test and train in torch7
    Former-commit-id: ab62067d3ea2ef85cb278de36eddee659b8d2aa6

---
+++ examples/lua/test.lua
@@ -0,0 +1,93 @@
+-- Eugenio Culurciello
+-- December 2016
+-- test a trained deep Q learning neural network 
+
+local base_path="/Users/eugenioculurciello/Desktop/ViZDoom/"
+package.path = package.path .. ";"..base_path.."lua/vizdoom/?.lua"
+require 'vizdoom.init'
+require 'nn'
+require 'image'
+
+torch.setnumthreads(8)
+torch.setdefaulttensortype('torch.FloatTensor')
+
+local opt = {...}
+opt.fpath = "results/model-10.net"--opt[1]
+if not opt.fpath then print('missing arg #1: missing network file to test!') return end
+
+-- load trained network:
+local model = torch.load(opt.fpath)
+
+ -- Doom configuration file path
+local config_file_path = base_path.."scenarios/simpler_basic.cfg"
+-- config_file_path = "../../scenarios/rocket_basic.cfg"
+-- config_file_path = "../../scenarios/basic.cfg"
+
+-- Doom game actions:
+local actions = {
+    [1] = torch.Tensor({1,0,0}),
+    [2] = torch.Tensor({0,1,0}),
+    [3] = torch.Tensor({0,0,1})
+}
+
+-- Other parameters
+local resolution = {30, 45} -- Y, X sizes of rescaled state / game screen
+
+-- Converts and down-samples the input image
+local function preprocess(inImage)
+  return image.scale(inImage, unpack(resolution))
+end
+
+-- Creates and initializes ViZDoom environment:
+function initializeViZdoom(config_file_path)
+    print("Initializing doom...")
+    game = vizdoom.DoomGame()
+    game:setViZDoomPath(base_path.."bin/vizdoom")
+    game:setDoomGamePath(base_path.."scenarios/freedoom2.wad")
+    game:loadConfig(config_file_path)
+    game:setMode(vizdoom.Mode.PLAYER)
+    game:setScreenFormat(vizdoom.ScreenFormat.GRAY8)
+    game:setScreenResolution(vizdoom.ScreenResolution.RES_640X480)
+    game:init()
+    print("Doom initialized.")
+    return game
+end
+
+function getQValues(state)
+    return model:forward(state)
+end
+
+function getBestAction(state)
+    local q = getQValues(state:float():reshape(1, 1, resolution[1], resolution[2]))
+    local max, index = torch.max(q, 1)
+    local action = index[1]
+    return action, q
+end
+
+-- Create Doom instance:
+local game = initializeViZdoom(config_file_path)
+
+-- Reinitialize the game with window visible:
+game:setWindowVisible(true)
+game:setMode(vizdoom.Mode.ASYNC_PLAYER)
+game:init()
+
+for i = 1, 20 do
+    game:newEpisode()
+    while not game:isEpisodeFinished() do
+        local state = preprocess(game:getState().screenBuffer:float():div(255))
+        local best_action_index = getBestAction(state)
+
+        game:makeAction(actions[best_action_index])
+        for j = 1, 12 do
+            game:advanceAction()
+        end
+    end
+
+    -- Sleep between episodes:
+    sys.sleep(1)
+    local score = game:getTotalReward()
+    print("Total score: ", score)
+end
+
+game:close()
