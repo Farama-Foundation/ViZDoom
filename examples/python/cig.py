ad7971d0a498cc47db15d7cc3c148bbad809f0a4
Marqt <marek@wydmuch.poznan.pl>
2017-03-24 02:58:03 +0100

    Updated Changelog.md

--- examples/python/cig.py
+++ examples/python/cig.py
@@ -18,7 +18,7 @@ game.add_game_args("-join 127.0.0.1") # Connect to a host for a multiplayer game
 
 # Name your agent and select color
 # colors: 0 - green, 1 - gray, 2 - brown, 3 - red, 4 - light gray, 5 - light brown, 6 - light red, 7 - light blue
-game.add_game_args("+name AI +colorset 0 +viz_debug 1")
+game.add_game_args("+name AI +colorset 0")
 
 # During the competition, async mode will be forced for all agents.
 game.set_mode(Mode.ASYNC_PLAYER)
