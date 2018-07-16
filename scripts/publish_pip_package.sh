57a080340fbf310a44a9e38a5b71f5e59f584172
mihahauke <kempka.michal@gmail.com>
2018-07-16 15:40:59 +0200

    Updated pip publish script.

--- scripts/publish_pip_package.sh
+++ scripts/publish_pip_package.sh
@@ -2,7 +2,6 @@
 
 python3 setup.py sdist
 NEWEST_PACKAGE=`ls dist/* | sort | tail -n 1`
-REPO="pypi"
-twine register ${NEWEST_PACKAGE} -r ${REPO}
-twine upload ${NEWEST_PACKAGE} -r ${REPO}
+
+twine upload ${NEWEST_PACKAGE}
 
