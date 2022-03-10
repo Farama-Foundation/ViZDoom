25f46c4d0fab5251236594c18eee8874aff088a5
mrzhuzhe <466648706@qq.com>
2022-03-10 14:17:58 +0800

    feat(ppo): remove unnessensary files

--- mytest/PPO/common/envs/__init__.py
+++
@@ -1,9 +0,0 @@
-from .envs.bit_flipping_env import BitFlippingEnv
-from .envs.identity_env import (
-    FakeImageEnv,
-    IdentityEnv,
-    IdentityEnvBox,
-    IdentityEnvMultiBinary,
-    IdentityEnvMultiDiscrete,
-)
-from .envs.multi_input_envs import SimpleMultiObsEnv
