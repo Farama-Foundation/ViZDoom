4c695001a4643d9abe3fe25dc9d5b4f9e7a35bc5
Anssi 'Miffyli' Kanervisto <kaneran21@hotmail.com>
2022-03-15 15:49:46 +0200

    Clean up code

--- gym_wrapper/gym_env_defns.py
+++ gym_wrapper/gym_env_defns.py
@@ -1,93 +1,13 @@
-import os 
+import os
 from vizdoom.gym_wrapper.base_gym_env import VizdoomEnv
 from vizdoom import scenarios_path
 
 
-class VizdoomBasic(VizdoomEnv):
+class VizdoomScenarioEnv(VizdoomEnv):
+    """Basic ViZDoom environments which reside in the `scenarios` directory"""
     def __init__(
-        self, frame_skip=1
+        self, scenario_file, frame_skip=1
     ):
-        super(VizdoomBasic, self).__init__(
-           os.path.join(scenarios_path, "basic.cfg"), frame_skip
-        )
-
-
-class VizdoomCorridor(VizdoomEnv):
-    def __init__(
-        self, frame_skip=1
-    ):
-        super(VizdoomCorridor, self).__init__(
-           os.path.join(scenarios_path, "deadly_corridor.cfg"), frame_skip
-        )
-
-
-class VizdoomDeathmatch(VizdoomEnv):
-    def __init__(
-        self, frame_skip=1
-    ):
-        super(VizdoomDeathmatch, self).__init__(
-           os.path.join(scenarios_path, "deathmatch.cfg"), frame_skip
-        )
-
-
-class VizdoomDefendCenter(VizdoomEnv):
-    def __init__(
-        self, frame_skip=1
-    ):
-        super(VizdoomDefendCenter, self).__init__(
-           os.path.join(scenarios_path, "defend_the_center.cfg"), frame_skip
-        )
-
-
-class VizdoomDefendLine(VizdoomEnv):
-    def __init__(
-        self, frame_skip=1
-    ):
-        super(VizdoomDefendLine, self).__init__(
-           os.path.join(scenarios_path, "defend_the_line.cfg"), frame_skip
-        )
-
-
-class VizdoomHealthGathering(VizdoomEnv):
-    def __init__(
-        self, frame_skip=1
-    ):
-        super(VizdoomHealthGathering, self).__init__(
-           os.path.join(scenarios_path, "health_gathering.cfg"), frame_skip
-        )
-
-
-class VizdoomHealthGatheringSupreme(VizdoomEnv):
-    def __init__(
-        self, frame_skip=1
-    ):
-        super(VizdoomHealthGatheringSupreme, self).__init__(
-           os.path.join(scenarios_path, "health_gathering_supreme.cfg"), frame_skip
-        )
-
-
-class VizdoomMyWayHome(VizdoomEnv):
-    def __init__(
-        self, frame_skip=1
-    ):
-        super(VizdoomMyWayHome, self).__init__(
-           os.path.join(scenarios_path, "my_way_home.cfg"), frame_skip
-        )
-
-
-class VizdoomPredictPosition(VizdoomEnv):
-    def __init__(
-        self, frame_skip=1
-    ):
-        super(VizdoomPredictPosition, self).__init__(
-           os.path.join(scenarios_path, "predict_position.cfg"), frame_skip
-        )
-
-
-class VizdoomTakeCover(VizdoomEnv):
-    def __init__(
-        self, frame_skip=1
-    ):
-        super(VizdoomTakeCover, self).__init__(
-           os.path.join(scenarios_path, "take_cover.cfg"), frame_skip
+        super(VizdoomScenarioEnv, self).__init__(
+           os.path.join(scenarios_path, scenario_file), frame_skip
         )
