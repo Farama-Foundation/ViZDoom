c7529d751ec09bbe845cec4953b771bc3e3e50c2
Anssi 'Miffyli' Kanervisto <kaneran21@hotmail.com>
2022-03-15 15:53:39 +0200

    Update example

--- examples/python/gym_wrapper.py
+++ examples/python/gym_wrapper.py
@@ -5,15 +5,13 @@
 import gym
 from vizdoom import gym_wrapper
 
-if __name__=='__main__':
-
-    env = gym.make("VizdoomHealthGatheringSupreme-v0", health=True)
+if __name__  == '__main__':
+    env = gym.make("VizdoomHealthGatheringSupreme-v0")
 
     # Rendering random rollouts for ten episodes
     for _ in range(10):
-        done = False 
+        done = False
         obs = env.reset()
         while not done:
             obs, rew, done, info = env.step(env.action_space.sample())
             env.render()
-
