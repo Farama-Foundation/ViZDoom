2255566c01a3b4424c6b78d94a20fcf70a3b2504
Marek Wydmuch <marek@wydmuch.poznan.pl>
2023-01-11 15:51:39 +0100

    Fix p_spec.cpp according to #336 and #530

--- src/vizdoom/src/p_spec.cpp
+++ src/vizdoom/src/p_spec.cpp
@@ -1269,7 +1269,7 @@ void P_InitSectorSpecial(sector_t *sector, int special, bool nothinkers)
 		if (sector->special >= Scroll_North_Slow &&
 			sector->special <= Scroll_SouthWest_Fast)
 		{ // Hexen scroll special
-			static const char hexenScrollies[24][2] =
+			static const signed char hexenScrollies[24][2] =
 			{
 				{  0,  1 }, {  0,  2 }, {  0,  4 },
 				{ -1,  0 }, { -2,  0 }, { -4,  0 },
