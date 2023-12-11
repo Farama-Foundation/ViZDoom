6e62e6f27307272c513d790bcd5e4c61d7d8f636
Marek Wydmuch <marek@wydmuch.poznan.pl>
2023-12-11 17:40:30 +0100

    Update the tests

--- tests/build_test_cibuildwheel_linux.sh
+++ tests/build_test_cibuildwheel_linux.sh
@@ -13,13 +13,15 @@ IMAGE_PREFIX="vizdoom_wheels"
 
 # Array in format "<base docker image> <base dockerfile to use> <additional commands to add to the dockerfile after FROM statement>"
 DOCKERFILES_TO_BUILD_AND_RUN=(
-    "almalinux:9 dnf-based.Dockerfile"  # Python 3.9
+    "almalinux:9 dnf-based.Dockerfile RUN dnf install -y 'dnf-command(config-manager)' && dnf config-manager --set-enabled crb"  # Python 3.9
     "fedora:36 dnf-based.Dockerfile"  # Python 3.10
     "fedora:37 dnf-based.Dockerfile"  # Python 3.11
-    "rockylinux:9 dnf-based.Dockerfile"  # Python 3.9
-    "debian:11.6 apt-based.Dockerfile ENV LANG C.UTF-8"  # Python 3.9
+    "rockylinux:9 dnf-based.Dockerfile RUN dnf install -y 'dnf-command(config-manager)' && dnf config-manager --set-enabled crb"  # Python 3.9
+    "debian:11 apt-based.Dockerfile ENV LANG C.UTF-8"  # Python 3.9
+    "debian:latest apt-based.Dockerfile ENV LANG C.UTF-8"  # Python 3.11
     "ubuntu:20.04 apt-based.Dockerfile"  # Python 3.8
     "ubuntu:22.04 apt-based.Dockerfile"  # Python 3.10
+    "ubuntu:latest apt-based.Dockerfile"  # Python 3.11
     "continuumio/miniconda3:latest conda-based.Dockerfile"  # Python 3.10
 )
 
--- tests/wheels_test_dockerfiles/conda-based.Dockerfile
+++ tests/wheels_test_dockerfiles/conda-based.Dockerfile
@@ -2,5 +2,7 @@ FROM continuumio/miniconda3:latest
 
 WORKDIR /vizdoom
 
+RUN conda install -y -c openal-soft
+
 COPY . ./
 CMD ["bash", "./scripts/install_and_test_wheel.sh"]
