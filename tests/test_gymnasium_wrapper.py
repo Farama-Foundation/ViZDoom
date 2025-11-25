3a7614a93d7f62684df9225d07c3bd0a6174676c
M. Elnimr <elnimr.m@gmail.com>
2025-11-25 12:07:08 -0800

    Fixing tests

--- tests/test_gymnasium_wrapper.py
+++ tests/test_gymnasium_wrapper.py
@@ -29,6 +29,9 @@ envs_with_animated_textures = [
 envs_with_audio = [
     "VizdoomBasicAudio",
 ]
+envs_with_notificatoins =[
+    "VizdoomBasicNotifications"
+    ]
 buffers = ["screen", "depth", "labels", "automap", "audio", "notifications"]
 
 
@@ -42,7 +45,7 @@ def test_gymnasium_wrapper():
         # Skip environments with animated textures and audio
         # as they might render different states for the same seeds
         # and audio might render slightly different
-        if env_name.split("-")[0] in envs_with_animated_textures + envs_with_audio:
+        if env_name.split("-")[0] in envs_with_animated_textures + envs_with_audio + envs_with_notifications:
             continue
 
         for frame_skip in [1, 4]:
