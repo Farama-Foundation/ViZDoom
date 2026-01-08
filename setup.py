cafdd9aa66b20c1fc40c540346bb25a24cf9e2e7
Hugo <hugo@hugohuang.com>
2026-01-08 07:37:54 +0000

    Corrected typo in filelock package name

--- setup.py
+++ setup.py
@@ -234,7 +234,7 @@ setup(
     extras_require={
         "test": ["pytest", "psutil"],
     },
-    install_requires=["numpy", "gymnasium>=0.28.0", "pygame-ce>=2.1.3", "lockfile"],
+    install_requires=["numpy", "gymnasium>=0.28.0", "pygame-ce>=2.1.3", "filelock"],
     python_requires=">=3.9.0,<3.15",
     packages=packages,
     package_dir={"": package_root},
