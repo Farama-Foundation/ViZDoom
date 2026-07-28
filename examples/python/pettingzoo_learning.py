0a448285a3d88cdc5022adbc7e748b21442b9dd9
lmbored <baokhoi136@gmail.com>
2026-07-28 23:56:55 +0200

    Increase eval episodes for less noise in frags/dmg logging

--- examples/python/pettingzoo_learning.py
+++ examples/python/pettingzoo_learning.py
@@ -590,7 +590,7 @@ def override_experiment_config(args, on_policy: bool, on_policy_minibatch_size: 
         "evaluation": True,  # Must be enabled for video logging
         "render": False,
         "evaluation_interval": args.rollout_steps * 25,
-        "evaluation_episodes": 1,
+        "evaluation_episodes": 2,
         "loggers": ["wandb"],
         "project_name": "benchmarl-vizdoom",
         "checkpoint_interval": args.rollout_steps * 100,
